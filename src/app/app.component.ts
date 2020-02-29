dbb737f71399add110a40ca1655b037cb7dd7d83
msomali <waleed92@live.com>
2020-03-01 02:06:36 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  // styleUrls: ['./success-alert/success-alert.component.css', './warning-alert/warning-alert.component.css']
 })
 export class AppComponent {
   title = 'my-first-app';
